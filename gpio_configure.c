d13622d56f3d69221c85a850540ba83c84829198
Guanxiong Fu <gufu3017@colorado.edu>
2018-02-22 20:55:14 -0700

    Update

---
+++ gpio_configure.c
@@ -0,0 +1,7 @@
+#include "nrf_gpio.h"
+#include "nrf.h"
+
+void gpio_init(){
+	nrf_gpio_range_cfg_output();
+
+}
---
+++ gpio_configure.h
@@ -0,0 +1,6 @@
+#ifndef GPIO_CONFIGURE_H__
+#define GPIO_CONFIGURE_H__
+
+#include "nrf.h"
+
+#endif /* GPIO_CONFIGURE_H__ */
